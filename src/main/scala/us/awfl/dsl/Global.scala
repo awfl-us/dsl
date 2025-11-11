8405f1a7e1773b16e49bf6db80046dc1837032f6
Paul <dezmoanded@gmail.com>
2025-11-10 21:02:28 -0500

    fix: trigger release this way

--- src/main/scala/us/awfl/dsl/Global.scala
+++ src/main/scala/us/awfl/dsl/Global.scala
@@ -1,6 +1,6 @@
 package us.awfl.dsl
 
-// Generalized placeholders resolved by yaml.Constants at generation time.
+// Generalized placeholders resolved by yaml.Constants at generation time
 val WORKFLOW_ID: Value[String] = str("WORKFLOW_ID")
 val WORKFLOW_EXECUTION_ID: Value[String] = str("WORKFLOW_EXECUTION_ID")
 val PROJECT_ID: Value[String] = str("PROJECT_ID")
